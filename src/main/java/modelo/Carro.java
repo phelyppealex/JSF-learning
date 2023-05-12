6a00df1729ab114c84b0e38bbd7168fd363d30d4
Phelyppe Álex <phelyppesilva@gmail.com>
2023-05-12 13:52:48 -0300

    configurando o banco

---
+++ src/main/java/modelo/Carro.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class Carro {
+	private int velocidade;
+	
+	public Carro(int velocidade) {
+		this.velocidade = velocidade;
+	}
+
+	public int getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(int velocidade) {
+		this.velocidade = velocidade;
+	}
+	
+	
+}
